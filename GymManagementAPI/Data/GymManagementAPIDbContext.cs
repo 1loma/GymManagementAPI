5df0f520e6876b82cb85d4e7a352a65248b30e14
lomad <lomad@L_WINDOWS11PRO>
2026-08-12 02:31:17 +0400

    Add project files.

---
+++ GymManagementAPI/Data/GymManagementAPIDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GymManagementAPI.Data
+{
+    public class GymManagementAPIDbContext : DbContext
+    {
+        public GymManagementAPIDbContext(DbContextOptions<GymManagementAPIDbContext> options) : base(options) { }
+    }
+}
---
+++ GymManagementAPI/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using GymManagementAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagementAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            //builder.Services.AddOpenApi();
+
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<GymManagementAPIDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "SomeName v1");
+                    options.RoutePrefix = "";
+                });
+
+
+                //app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
